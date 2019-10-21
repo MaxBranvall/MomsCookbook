6fadbb08328125364cff895cfa80ec9f7d999db2
[Max Branvall] <maxbranvall@gmail.com>
2019-10-21 16:49:09 -0400

    Removed hard coding from recipe list. Refactored stylesheet.

--- Client/src/app/recipe-list/recipe-list.component.ts
+++ Client/src/app/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Entry } from '../Models/Entry';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,12 +10,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RecipeListComponent implements OnInit {
 
-  title;
-
   constructor(private route: ActivatedRoute,) { }
 
   ngOnInit() {
     this.title = this.route.snapshot.paramMap.get('categoryTitle').toString();
+    this.initTimes();
+  }
+
+  title;
+  recipeSorted: boolean = false;
+  prepTimeSorted: boolean = false;
+  cookTimeSorted: boolean = false;
+  reverseSort = false;
+  orderByField="Name";
+
+  model = new Entry("Breaded Chicken Cutlet",
+  '../../assets/placeholders/recipeplaceholder.jpg', null, null, 0, 5, 1, 45, null, null, null);
+  model2 = new Entry("Areaded Chicken Cutlet",
+  '../../assets/placeholders/recipeplaceholder.jpg', null, null, 1, 30, 0, 20, null, null, null);
+
+  entries = [this.model, this.model2];
+
+  initTimes()
+  {
+    this.entries.forEach(e => {
+      e.PrepTime = this.makeTime(e.PrepTimeH, e.PrepTimeM);
+      e.CookTime = this.makeTime(e.CookTimeH, e.CookTimeM);
+    });
+  }
+
+  makeTime(hour: number, minute: number) : string
+  {
+    return hour + ':' + minute;
   }
 
 }
