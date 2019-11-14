de70269de9fa3f5f9bff7ebe6bace2be71f1411c
[Max Branvall] <maxbranvall@gmail.com>
2019-11-13 20:47:27 -0500

    Refactored and added documentation.

--- Client/src/app/Services/recipe.service.ts
+++ Client/src/app/Services/recipe.service.ts
@@ -1,53 +1,66 @@
 import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
-import { tap, map} from 'rxjs/operators';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { Entry } from '../Models/Entry';
 import { Recipe } from '../Models/Recipe';
-import { ENTRIES } from '../Models/Mock-Entries';
-import { ObserveOnSubscriber } from 'rxjs/internal/operators/observeOn';
-import { Identifiers } from '@angular/compiler/src/render3/r3_identifiers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  private apiUrl = "https://localhost:5001/api/"
-  httpOptions = {
-    // headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    headers: new HttpHeaders({'Content-Type': 'image/jpeg'})
-  };
+  // private apiUrl = "https://localhost:5001/api/";
+  private apiUrl = "http://192.168.1.45:5000/api/";
 
   constructor(
     private http: HttpClient,
   ) { }
 
   getAllEntries(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.apiUrl + 'recipe');
+    /*
+      Gets all recipes meta info from database.
+     */
+    return this.http.get<Recipe[]>(this.apiUrl + 'Recipes');
   }
 
-  getEntry(Name : string): Observable<Entry[]> {
-    return this.http.get<Entry[]>(this.apiUrl + 'recipe/' + Name);
+  getEntry(Name : string): Observable<Entry> {
+    /*
+      Retrieves a FULL recipe by it's name.
+      TODO: Get recipe by UID instead for scalability.
+     */
+    return this.http.get<Entry>(this.apiUrl + 'Recipes/' + Name, {reportProgress: true});
   }
 
-  addRecipe(s: Entry): Observable<number> {
-    return this.http.post<number>(this.apiUrl + "Recipe", s);
+  addRecipe(entry: Entry): Observable<number> {
+    /*
+      Adds a new recipe to the database.
+     */
+    return this.http.post<number>(this.apiUrl + "Recipes", entry);
+  }
+
+  addDownloadURL(downloadURL: string, id: number)
+  {
+    /*
+      Replaces Recipe image path with the download URL
+      from Firebase.
+     */
+    return this.http.put(this.apiUrl + "Recipes", {id, downloadURL}, {reportProgress: true, observe: "events"});
   }
 
   addPhoto(file: any, ID: string) {
+
+    /*
+      Adds photo to local file system.
+      Migrated to Firebase, but leaving this here
+      in case I want to use it for any reason.
+     */
+
     let formModel= new FormData();
     formModel.append("File", file);
     formModel.append("ID", ID);
 
     return this.http.post(this.apiUrl + "Media", formModel);
   }
-
-  // addRecipe(recipe: string): Observable<string> {
-  //   return this.http.post<string>(this.apiUrl + "recipe", recipe, this.httpOptions)
-  // };
-
-  // test(s: string): Observable<string
 
 }
