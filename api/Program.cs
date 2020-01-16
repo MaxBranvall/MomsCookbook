21fbb035c4759df8b1e8bda5593c3f0c9c672fb5
[Max Branvall] <maxbranvall@gmail.com>
2020-01-15 20:55:44 -0500

    Added Serilog and cleaned up imports in program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
+using Serilog;
 
 namespace api
 {
