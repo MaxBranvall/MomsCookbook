463fec7488b69a32a0a0f9a1913b35427c3d9797
Maxwell J Branvall <36718512+MaxBranvall@users.noreply.github.com>
2020-05-03 21:44:31 -0400

    Removed First and last name properties.

--- api/Entities/Users.cs
+++ api/Entities/Users.cs
@@ -4,8 +4,6 @@ namespace api.Entities
     public class Users
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
