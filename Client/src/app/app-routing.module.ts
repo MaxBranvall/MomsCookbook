8533d21cb4aa7b17b010cd1360b3f7825742c5b1
Maxwell J Branvall <36718512+MaxBranvall@users.noreply.github.com>
2020-05-17 04:46:54 -0400

    Implemented auth guard into routes

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -7,14 +7,41 @@ import { RecipeEntryComponent } from './recipe-entry/recipe-entry.component';
 import { LoginComponent } from './login/login.component';
 import { EntryMode } from './_helpers/entry-mode.enum';
 import { AuthGuard } from './_guards/auth.guard';
+import { Role } from './_helpers/role.enum';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
-  { path: 'newentry', component: RecipeEntryComponent, data : {mode : EntryMode.NewEntry}},
-  { path: 'login', component: LoginComponent },
-  { path: 'editrecipe', component: RecipeEntryComponent, data : {mode : EntryMode.EditEntry} },
-  { path: ':categoryTitle', component: RecipeListComponent },
-  { path: ':categoryTitle/:recipeName', component: RecipePageComponent },
+  {
+    path: '',
+    component: HomePageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'newentry',
+    component: RecipeEntryComponent,
+    canActivate: [AuthGuard],
+    data :
+    {
+      mode : EntryMode.NewEntry,
+      roles: [Role.Admin]
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'editrecipe',
+    component: RecipeEntryComponent,
+    data : {mode : EntryMode.EditEntry}
+  },
+  {
+    path: ':categoryTitle',
+    component: RecipeListComponent
+  },
+  {
+    path: ':categoryTitle/:recipeName',
+    component: RecipePageComponent
+  },
 ];
 
 @NgModule({
