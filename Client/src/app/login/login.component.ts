f07bb2f9c10d4dfd805513c9b74309534cde51af
Maxwell J Branvall <36718512+MaxBranvall@users.noreply.github.com>
2020-05-17 04:49:38 -0400

    Started creating create accout layout

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticateModel } from '../Models/AuthenticateModel';
+import { AuthenticationService } from '../Services/authentication.service';
 import { Users } from '../Entities/Users';
+import { Role } from '../_helpers/role.enum';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +13,28 @@ import { Users } from '../Entities/Users';
 })
 export class LoginComponent implements OnInit {
 
-  private user: Users;
+  private newAccount: Users;
+  private model: AuthenticateModel;
+  private toggleCreateAccount = false;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
 
-    this.user = new Users();
+    this.model = new AuthenticateModel();
+    this.newAccount = new Users();
 
+  }
+
+  get diagnostic()
+  {
+    return JSON.stringify(this.newAccount);
+  }
+
+  onSubmit() {
+    this.authService.login(this.model.username, this.model.password).subscribe(x => {
+      this.router.navigate(['/']);
+    });
   }
 
 }
